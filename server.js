46781da146462012bacb5adc24c9a35c1acbf1be
Michelle <michelle.douma@gmail.com>
2022-07-15 18:16:17 -0700

    clearing tree

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = requite('express');
+const app = express();
+
+var variable = example();
+//needing to make false code to try and fix git issue
+
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API is on 3001`);
+});
