3d862485a3449da33a00e0edd72502d7b828c384
Michelle <michelle.douma@gmail.com>
2022-09-17 13:00:37 -0400

    adding routes

---
+++ Develop/apiRoutes/index.js
@@ -0,0 +1,8 @@
+//sets the use of the router
+const router = require('express').Router();
+const notesRoute = require('../apiRoutes/notes');
+
+//sets the use of the note router
+router.use(notesRoute);
+
+module.exports = router; 
