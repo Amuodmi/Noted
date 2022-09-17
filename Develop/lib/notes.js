99261eb14e29c2439eacd957266312aa443a4770
Michelle <michelle.douma@gmail.com>
2022-09-17 12:57:20 -0400

    updating notes.js

---
+++ Develop/lib/notes.js
@@ -0,0 +1,21 @@
+//need for Node functions, FS, Path, and database
+const fs = require('fs');
+const { notes } = require('../db.db.json');
+const path = require('path');
+
+//a function that will create a new note
+//body and notes will be pushed to array
+function newNote(body, notes){
+    const note = body;
+    notes.push(note);
+
+    
+//FS will write the file to db.json
+fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({notes}, null, 2)
+);
+return note;
+}
+
+module.exports = newNote; 
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const router = require('express').Router();
 const app = express();
+const PORT = process.env.PORT || 3001;
 
 const { notes } = require('./')
 
@@ -7,23 +9,34 @@ app.get('/db/db'), (req,res) => {
     res.send('data is connected')
 }
 
+//GET /api/notes should read the db.json file and return all saved notes as JSON
+app.use(express.json());
 
-//db.json is used to store and retrieve notes using the fs module
+//parses incoming string of info, or array data
+app.use(express.urlencoded({ extended: true }));
+
+//parses JSON data that comes in
+app.use(express.static('public'));
+
+
+//Use api routes
+app.use('/api', apiRoutes);
+
 
 //GET /notes should return the notes.html file
 //GET * should return the index.html file
 
-//GET /api/notes should read the db.json file and return all saved notes as JSON
+
+
 //POST /api/notes should receive a new note to save on the request body, add it to the 
 //db.json file, and then return the new note to the client. 
 
 //You'll need to find a way to give each note a unqiue ID when it's saved (look into npm packages that could do this for you)
 //notes = 
 
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
 
-app.listen(3001, () => {
-    console.log(`API is on 3001`);
-});
+
 
 //BONUS: DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order
 //to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property
